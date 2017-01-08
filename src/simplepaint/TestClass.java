208d407184c2616870e0c6c81c4961d316ca5b14
Amanda Braun <Ac_br_aun@hotmail.com>
2017-01-07 20:36:17 -0600

    improved paint interface

--- src/simplepaint/TestClass.java
+++ src/simplepaint/TestClass.java
@@ -29,18 +29,47 @@ import javax.swing.JPanel;
 public class TestClass extends JFrame implements ActionListener {
 
     DrawingPanel dp;
+    JButton load, black, red, blue, green, clear;
 
     public TestClass() {
-        setSize(400, 400);
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
+        //Set color, clear and send image buttons
         JPanel top = new JPanel();
-        JButton load = new JButton("Send Image");
+        black = new JButton("Black");
+        black.setBackground(Color.BLACK);
+        black.setForeground(Color.WHITE);
+        red = new JButton("Red");
+        red.setBackground(Color.RED);
+        red.setForeground(Color.WHITE);
+        green = new JButton("Green");
+        green.setBackground(Color.GREEN);
+        green.setForeground(Color.WHITE);
+        blue = new JButton("Blue");
+        blue.setBackground(Color.BLUE);
+        blue.setForeground(Color.WHITE);
+        clear = new JButton("Clear");
+        clear.setBackground(Color.DARK_GRAY);
+        clear.setForeground(Color.WHITE);
+        load = new JButton("Send Image");
+        
         load.addActionListener(this);
+        black.addActionListener(this);
+        red.addActionListener(this);
+        green.addActionListener(this);
+        blue.addActionListener(this);
+        clear.addActionListener(this);
+        
+        top.add(black);
+        top.add(red);
+        top.add(green);
+        top.add(blue);
+        top.add(clear);
         top.add(load);
-
+        
         dp = new DrawingPanel();
-        dp.setBackground(Color.CYAN);
+        dp.setBackground(Color.WHITE);
 
         add(top, BorderLayout.NORTH);
         add(dp, BorderLayout.CENTER);
@@ -53,9 +82,27 @@ public class TestClass extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        BufferedImage bi = dp.getScreenShot();
-        //return bi;
-        //dp.loadDrawing(bi);
+        
+        if(ae.getSource() == load)
+        {
+          BufferedImage bi = dp.getScreenShot();
+        }
+        else if(ae.getSource() == black)
+        {
+          dp.setDrawColor(Color.BLACK);
+        }
+        else if(ae.getSource() == red)
+        {
+            dp.setDrawColor(Color.RED);
+        }
+        else if(ae.getSource() == green)
+        {
+            dp.setDrawColor(Color.GREEN);
+        }
+        else if(ae.getSource() == blue)
+        {
+            dp.setDrawColor(Color.BLUE);
+        }
     }
     
 }
